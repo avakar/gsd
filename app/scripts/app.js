4f50ee737ae6cdde75e489c737b38cf6be78acfd
Martin Vejnár <avakar@ratatanek.cz>
2014-12-14 22:10:42 +0100

    Support arrow-navigable.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,19 +3,74 @@
 var app = angular.module('myapp2App', ['ui.bootstrap', 'ui.sortable']);
 
 app.directive('arrowNavigable', function() {
+
+    function findprev(node) {
+        do {
+            var prev = node.previousSibling;
+            if (prev === null) {
+                while (prev === null) {
+                    node = node.parentNode;
+                    if (node === null) {
+                        return null;
+                    }
+                    prev = node.previousSibling;
+                }
+            }
+
+            do {
+                node = prev;
+                prev = node.lastChild;
+            } while (prev !== null);
+        } while (node.nodeType !== Node.ELEMENT_NODE || !node.hasAttribute('arrow-navigable'));
+
+        return node;
+    }
+
+    function findnext(node) {
+        do {
+            var prev = node.nextSibling;
+            if (prev === null) {
+                while (prev === null) {
+                    node = node.parentNode;
+                    if (node === null) {
+                        return null;
+                    }
+                    prev = node.nextSibling;
+                }
+            }
+
+            do {
+                node = prev;
+                prev = node.firstChild;
+            } while (prev !== null);
+        } while (node.nodeType !== Node.ELEMENT_NODE || !node.hasAttribute('arrow-navigable'));
+
+        return node;
+    }
+
+    function refocus(src, dest) {
+        if (dest === null)
+            return;
+
+        var pos = src.selectionStart;
+        dest.focus();
+        dest.selectionStart = pos;
+        dest.selectionEnd = pos;
+    }
+
     return {
         link: function(scope, elem, attrs, controller) {
             elem.on('keydown', function(e) {
-                if (e.keyCode == 38) {
-                    alert('test');
+                if (e.keyCode === 38) {
+                    refocus(this, findprev(this));
                     e.preventDefault();
-                } else if (e.keyCode == 40) {
-                    alert('testdown');
+                } else if (e.keyCode === 40) {
+                    refocus(this, findnext(this));
                     e.preventDefault();
                 }
-            })
+            });
         }
-    }
+    };
 });
 
 app.controller('tasklistController', function($scope) {
